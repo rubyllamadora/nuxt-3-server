6aff3382a870d120296a5161de1e938ee3a2254b
rubyllamadora <ruby.lamadora@gmail.com>
2023-12-14 10:14:54 +1100

    Setup Nuxt and add test page and server API

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup>
+const { data: scizor } = await useFetch('/api/scizor')
+</script>
+<template>
+  <h1>Home Page for Nuxt 3 Server</h1>
+  <ul>
+    <li>
+      <img :src="scizor.sprite">
+    </li>
+    <li>{{ scizor.id }}: {{ scizor.name }}</li>
+  </ul>
+</template>
